64b3f50c51891a2ed1b361b7997f7ff669285b81
OliverW10 <oliver@warrickfamily.com>
2020-06-18 10:50:47 +1000

    finished code for two lasers

--- break-beam/break-beam.ino
+++ break-beam/break-beam.ino
@@ -3,16 +3,15 @@
 
 #define IO_USERNAME "Olikat"
 #define FEED_NAME "Inside"
-#define IO_KEY "cant put on github"
-#define WIFI_SSID "DrB"
-#define WIFI_PASS ""
+#define IO_KEY "cant put"
+#define WIFI_SSID "FBI Watch Van #6"
+#define WIFI_PASS "aaaaaaaa"
 
 
 
 AdafruitIO_WiFi  io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 AdafruitIO_Feed *output = io.feed(FEED_NAME);
 
-int ledPin = 5;
 int sensPins[2] = {4, 0};
 int sensVals[2] = {0, 0};
 
@@ -20,18 +19,22 @@ int recordLength = 200;
 int dataLog[2][200];
 long total = 0;
 int average[2] = {0, 0};
-bool covered[2] = {false, false};
+
 int entered = 0;
+int left = 0;
+int inside = 0;
+
+int maxTime = 1000; // the maximum time between the sensors being covered that will consider it an enternace
 
 unsigned long time1 = millis();
 int outputCounter = 0;
 int outputWaitTime = 50; // how long to wait between sending to adafruit in cyces
 
-int coveredTimes[2] = {100, 100}; // loops since the sensor was covered
+int coveredTimes[2] = {maxTime, maxTime}; // loops since the sensor was covered
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(sensPin, INPUT);
+  pinMode(sensPins[0], INPUT);
+  pinMode(sensPins[1], INPUT);
   Serial.begin(9600);
 
   io.connect();
@@ -43,57 +46,49 @@ void setup() {
 }
 
 void loop() {
+  // run the adafruit thing
   io.run();
 
-  // Read the light values from the sensors
-  sensVals[0] = analogRead(sensPins[0]);
-  sensVals[1] = analogRead(sensPins[1]);
+  for(int sensor = 0; sensor < 2; sensor += 1){
+    // Read the light value form the sensor
+    sensVals[sensor] = analogRead(sensPins[sensor]);
 
-  // Get average of last recordLength readings and update log of data
-  total = 0;
-  for(int i = recordLength-2; i>=0; i--){
-    dataLog[0][i+1] = dataLog[0][i];
-    total += dataLog[0][i];
-  }
-  average[0] = total/recordLength;
-  dataLog[0][0] = sensVal;
+    // Add up and average the last recordLength values
+    // and moves the values backwards one
+    total = 0;
+    for(int i = recordLength-2; i>=0; i--){
+      dataLog[sensor][i+1] = dataLog[sensor][i];
+      total += dataLog[sensor][i];
+    }
+    average[sensor] = total/recordLength;
+    // set the begining of the chain to the sensor value
+    dataLog[sensor][0] = sensVals[sensor];
 
-  // Get average of last recordLength readings and update log of data, again
-  total = 0;
-  for(int i = recordLength-2; i>=0; i--){
-    dataLog[1][i+1] = dataLog[1][i];
-    total += dataLog[1][i];
-  }
-  average[1] = total/recordLength;
-  dataLog[1][0] = sensVal;
-
-  if((average - sensVal) > 100 && covered == false){
-    Serial.println("crossed one");
-    entered += 1;
-    coveredTimes[0] = 0;
-  }
-  if((average - sensVal) > 100 && covered == false){
-    Serial.println("crossed one");
-    entered += 1;
-    coveredTimes[1] = 0;
+    // Check if the sensor is considered covered and set the times since covered to zero
+    if((average[sensor] - sensVals[sensor]) > 100){
+      coveredTimes[sensor] = 0;
+    }
+    coveredTimes[sensor] ++;
   }
 
-  coveredTimes[0] ++;
-  coveredTimes[1] ++;
+  if(coveredTimes[0] < maxTime && coveredTimes[1] < maxTime){
+    Serial.println("Passed");
+     if(coveredTimes[0] < coveredTimes[1]){ // enterance
+      entered += 1;
+      inside += 1;
+     }else{ //exit
+      left += 1;
+      inside -= 1;
+     }
+  }
   
-
+  // send to adafruit every thousand milliseconds
   unsigned long time2 = millis();
   if(abs(time1 - time2) > 1000){
     unsigned long time1 = millis();
-    Serial.println(entered);
-      output -> save(entered); 
+    Serial.println(inside);
+      output -> save(inside); 
   }
   
-  covered = {(average[0] - sensVal[0]) > 100, (average[1] - sensVal[1]) > 100};
-  if(covered == true){
-    digitalWrite(ledPin, HIGH);
-  }else{
-    digitalWrite(ledPin, LOW);
-  }
   delay(10);
 }
